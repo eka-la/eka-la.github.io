f5729b93c033b28b0887523f4fb55bc8935dc75a
David Arnold <dgx.arnold@gmail.com>
2024-02-03 15:30:13 +0100

    feat: select fonts and style

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,13 @@ export default defineConfig({
         baseUrl:
           "https://github.com/eka-la/eka-la.github.io/edit/main/src/content/docs/",
       },
+      customCss: [
+        // Font sourec for normal and semi bold specimens.
+        "@fontsource/fira-sans/400.css",
+        "@fontsource/fira-sans/600.css",
+        // Relative path to custom CSS file
+        "./src/styles/custom.css",
+      ],
       sidebar: [
         {
           label: "Guides",
