7e5f3e45f7c5a20e8adabe5c51482d78c2ecee5c
David Arnold <dgx.arnold@gmail.com>
2024-02-03 13:36:44 +0100

    chore: modernize std

--- flake.nix
+++ flake.nix
@@ -1,15 +1,16 @@
 {
-  inputs.std.url = "github:divnix/std/nixpkgs-config";
+  inputs.std.url = "github:divnix/std";
   inputs.nixpkgs.follows = "std/nixpkgs";
+  inputs.std.inputs.devshell.url = "github:numtide/devshell";
   outputs = inputs @ {std, ...}:
     std.growOn {
       inherit inputs;
       nixpkgsConfig.allowUnfree = true;
       systems = ["x86_64-linux"];
       cellsFrom = ./nix;
-      cellBlocks = [
-        (std.installables "packages")
-        (std.devshells "devshells")
+      cellBlocks = with std.blockTypes; [
+        (installables "packages")
+        (devshells "devshells")
       ];
     };
 }
