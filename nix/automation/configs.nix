b236294d9062379ca27a1c4fb2ac083ce6392c34
David Arnold <dgx.arnold@gmail.com>
2024-02-03 14:55:00 +0100

    feat: init astro with starlight docs theme

--- nix/automation/configs.nix
+++ nix/automation/configs.nix
@@ -1,9 +1,18 @@
 let
   inherit (inputs) nixpkgs;
+  inherit (inputs.std.dmerge) append;
   inherit (inputs.std.data) configs;
   inherit (inputs.std.lib.dev) mkNixago;
 in {
-  treefmt = (mkNixago configs.treefmt) {};
+  treefmt = (mkNixago configs.treefmt) {
+    data.formatter.prettier = {
+      excludes = [
+        "./package.json"
+        "./pnpm-lock.yaml"
+      ];
+      includes = append ["*.mjs"];
+    };
+  };
   editorconfig = (mkNixago configs.editorconfig) {};
   conform = (mkNixago configs.conform) {};
   lefthook = (mkNixago configs.lefthook) {};
